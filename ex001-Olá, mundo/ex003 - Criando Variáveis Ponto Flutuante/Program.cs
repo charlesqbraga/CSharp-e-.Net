8c28d6863c80c6215dcb1668c0716b1acf9f840c
Charles Braga <charlesqbraga.dev@gmail.com>
2023-07-24 20:48:39 -0300

    Exercicio 2

---
+++ ex001-Olá, mundo/ex003 - Criando Variáveis Ponto Flutuante/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+        double salario;
+        salario = 3000.40;
+        
+        Console.WriteLine(salario);
+        Console.WriteLine("Tecle Enter para fechar..");
+        Console.ReadLine();
+    }
+}
