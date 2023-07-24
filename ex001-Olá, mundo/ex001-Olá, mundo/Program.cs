18b6729075b6f4e658c6ad29d43d9db68a781195
Charles Braga <charlesqbraga.dev@gmail.com>
2023-07-24 17:02:16 -0300

    Olá mundo

---
+++ ex001-Olá, mundo/ex001-Olá, mundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá Mundo");
+        Console.WriteLine("Tecle Enter para fechar..");
+        Console.ReadLine();
+        Console.ReadLine();
+    }
+} 
