cae3d003416fb7525a8513ecaab5c74688e52b0f
Charles Braga <charlesqbraga.dev@gmail.com>
2023-07-25 02:21:55 -0300

    else if

---
+++ ex001-Olá, mundo/ex007 - Condicionais/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 07 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadePessoa = 1;
+
+
+        if (idadeJoao >= 18)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else  
+        {
+            if (quantidadePessoa >= 1)
+                Console.WriteLine("Ele(a) está acompanhado(a), pode entrar.");
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+
+            }
+            
+        }
+
+
+        Console.WriteLine("Tecle Enter para fechar..");
+        Console.ReadLine();
+    }
+}
+
