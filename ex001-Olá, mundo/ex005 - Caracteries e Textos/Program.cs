8b61e740d8a954e47cc39a5787149bdd67b60730
Charles Braga <charlesqbraga.dev@gmail.com>
2023-07-25 01:26:30 -0300

    Atribuindo variáveis

---
+++ ex001-Olá, mundo/ex005 - Caracteries e Textos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeo 05 - Caractéries e textos.");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        string primeiraFrase = "Olá, mundo!!! ";
+        Console.WriteLine(primeiraFrase + 2023);
+
+        string cursos = @"Cursos disponiveis:
+- C#
+- Python
+- JavaScript
+- Java";
+        Console.WriteLine(cursos);
+
+
+
+        Console.WriteLine("Tecle Enter para fechar..");
+        Console.ReadLine();
+    }
+}
---
+++ ex001-Olá, mundo/ex006 - Atribuições de Variáveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 06 - Atribuições de variáveis.");
+
+        int idade = 30;
+        int idadeAna = idade;
+        Console.WriteLine(idadeAna);
+
+
+        idade = 60;
+        Console.WriteLine(idade);
+
+        Console.WriteLine("Tecle Enter para fechar..");
+        Console.ReadLine();
+    }
+}
