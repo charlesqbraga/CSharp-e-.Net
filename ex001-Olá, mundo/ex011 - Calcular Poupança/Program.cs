ee1f1a2f7ba33cbd57d065304555dd0b9a59c2d1
Charles Braga <charlesqbraga.dev@gmail.com>
2023-07-26 01:53:42 -0300

    While

---
+++ ex001-Olá, mundo/ex011 - Calcular Poupança/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 11 - Calcula Poupança");
+
+        double investimento = 1000;
+         //rendimento de 0.5% (0,005) ao mês.
+
+         /*investimento = investimento + investimento * 0.005;
+         Console.WriteLine(investimento);*/
+
+        int mes = 1;
+
+        while (mes <= 12)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("No mês " +mes + " você tem R$" + investimento);
+            mes++;
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
