2b8b9cee419037e323e66f693def5a964850bf58
Charles Braga <charlesqbraga.dev@gmail.com>
2023-07-26 00:55:08 -0300

    Condições booleanas

--- ex001-Olá, mundo/ex008 - Condicionais II/Program.cs
+++ ex001-Olá, mundo/ex008 - Condicionais II/Program.cs
@@ -7,28 +7,25 @@ class Programa
         Console.WriteLine("Executando o projeto 07 - Condicionais");
 
         int idadeJoao = 16;
-        int quantidadePessoa = 1;
+        int quantidadePessoa = 2;
+
+        bool acompanhado = quantidadePessoa > 1;
+        bool grupo = true;
 
 
-        if (idadeJoao >= 18)
+        if (idadeJoao >= 18 || acompanhado)
         {
             Console.WriteLine("Pode entrar!");
         }
         else
         {
-            if (quantidadePessoa >= 1)
-                Console.WriteLine("Ele(a) está acompanhado(a), pode entrar.");
-            else
-            {
-                Console.WriteLine("Não pode entrar");
-
-            }
-
+            Console.WriteLine("Não pode entrar");
         }
+
+
 
 
         Console.WriteLine("Tecle Enter para fechar..");
         Console.ReadLine();
     }
 }
-
