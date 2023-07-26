2612b874f4b563ade4ca2d47508b5a664ca790a9
Charles Braga <charlesqbraga.dev@gmail.com>
2023-07-26 02:01:06 -0300

    For

---
+++ ex001-Olá, mundo/ex012 - Calcular Poupança II/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 11 - Calcula Poupança");
+
+        double investimento = 1000;
+        //rendimento de 0.5% (0,005) ao mês.
+
+        for (int mes = 1;  mes <= 12; mes++)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("No mês " + mes + " você tem R$" + investimento);
+           
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
